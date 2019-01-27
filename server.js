ceb102b410a7d39929e796e36cd3daae43020200
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-01-26 23:49:29 -0500

    express boilerplate and mlab connection

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+
+const profile = require("./routes/api/profile");
+
+const posts = require("./routes/api/posts");
 
 const app = express();
 
+// mlab connection config
+const db = require("./config/keys").mongoURI;
+
+// connect to mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to mlab"))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello"));
+
+// routes
+app.use("./routes/api/users", users);
+app.use("./routes/api/profile", profile);
+app.use("./routes/api/posts", posts);
 
 const port = process.env.port || 4000;
 
